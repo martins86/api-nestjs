16604ef512d573eaf8662468eacc14d24f7b73d6
martins86 <martins86@gmail.com>
2024-03-23 21:30:56 +0000

    feat: add validator email unique

---
+++ src/user/validators/email-is-unique.validator.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+import {
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+
+import { UserRepository } from 'src/user/user.repository';
+
+@Injectable()
+@ValidatorConstraint({ async: true })
+export class EmailIsUniqueValidator implements ValidatorConstraintInterface {
+  constructor(private userRepository: UserRepository) {}
+
+  async validate(email: string): Promise<boolean> {
+    const validateEmail = await this.userRepository.findEmail(email);
+    return !validateEmail;
+  }
+}
+
+export const EmailIsUnique = (optionsValidation: ValidationOptions) => {
+  return (obj: object, prop: string) => {
+    registerDecorator({
+      target: obj.constructor,
+      propertyName: prop,
+      options: optionsValidation,
+      constraints: [],
+      validator: EmailIsUniqueValidator,
+    });
+  };
+};
